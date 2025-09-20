8d75d69df56ebfd4ecd79ba5b1bee8b9801ba077
agospascucci <pascucciagostina@gmail.com>
2025-09-20 17:59:05 -0300

    Cambio de nombre a Backend

--- react-ui-lib/libreria+persistencia/backend-mi-libreria/prisma/migrations/20250829130229_nationality_op/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Author" ALTER COLUMN "nationality" DROP NOT NULL;
--- react-ui-lib/libreria+persistencia/backend-mi-libreria/prisma/migrations/20250907221048_nationalitynotoptional/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `nationality` on table `Author` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Author" ALTER COLUMN "nationality" SET NOT NULL;
