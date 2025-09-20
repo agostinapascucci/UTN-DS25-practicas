2acf69c0421a256062772b42a41979e54e630e63
agospascucci <pascucciagostina@gmail.com>
2025-09-20 18:02:12 -0300

    Reestructuracion

--- react-ui-lib/libreria+persistencia/frontend/vite.config.js
+++ react-ui-lib/libreria+persistencia/frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,          // equivale a 0.0.0.0
+    port: 5173,
+    strictPort: true,
+    hmr: { clientPort: 5173 } // ayuda en algunos entornos
+  }
+
 })
+
