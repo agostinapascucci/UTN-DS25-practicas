3cdc34dd1f98b673b3f771cf51d45e87549a5774
agospascucci <pascucciagostina@gmail.com>
2025-10-08 21:20:09 -0300

    Renombrar

--- react-ui-lib/libreria-persistencia/backend/src/config/prisma.ts
+++
@@ -1,8 +0,0 @@
-import {PrismaClient} from '../generated/prisma';
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
-const prisma = globalForPrisma.prisma ?? new PrismaClient({
-    log: ["error", "warn", "query"], // agregamos "query" para debuggear
- });
- 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-export default prisma;
