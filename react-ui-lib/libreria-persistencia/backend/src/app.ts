64e4087b7ac9b757bf6ef671cd529e386b473eba
agospascucci <pascucciagostina@gmail.com>
2025-10-14 20:49:28 -0300

    141025 - Backend - Arreglo del APP.TS

--- react-ui-lib/libreria-persistencia/backend/src/app.ts
+++ react-ui-lib/libreria-persistencia/backend/src/app.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // Opciones de CORS
 const corsOptions = {
-origin: [ process.env.FRONTEND_URL_LOCAL, process.env.FRONTEND_URL_PROD ],
+origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
